620a14451d6b8b2b3934be2e1e04295f3266f96d
Sergey Vlasov <one@sysint.net>
2021-06-07 16:18:23 +0300

    Removed garbage

--- index.php
+++ index.php
@@ -6,17 +6,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-/*use Hotcoffee\Coffee;
-use Hotcoffee\CoffeeSettings;
-use Hotcoffee\Country\Italy;
-
-$italy = new Italy();
-$coffeeSettings = new CoffeeSettings();
-
-$coffee = new Coffee($italy, $coffeeSettings);
-
-$coffee->make();*/
-
 use Hotcoffee\App\Template\Homepage;
 use Hotcoffee\App\Bootstrap;
 
